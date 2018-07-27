2dc754e6dcca87ae302ce33a976a9c8acc0c8429
Tran Phong <tranphong96.hbk@gmail.com>
2018-07-27 00:33:38 -0700

    Fixing the understandings of action values in bandit.py

--- Chapter2/bandit.py
+++ Chapter2/bandit.py
@@ -3,31 +3,19 @@ import random
 import matplotlib.pyplot as plt
 from operator import attrgetter
 
+# we don't know the true value of the bandit. we need to choose which bandit to maximize the rewards received
 class Bandit:
     def __init__(self, variance, size, stationary):
         self.values = np.random.normal(loc = 0, scale = variance, size = size)
-        self.rewards = np.random.normal(loc = np.random.choice(self.values), scale = 1, size = size)
-        self.value = 0
-        self.count = 0
+        self.true_value = 0
+        #self.rewards = np.random.normal(loc = self.true_value, scale = 1, size = size)
         self.stationary = stationary
         self.size = size
 
     def get_reward(self):
-        if not self.stationary:
-            self.values += np.random.normal(loc = 0, scale = 0.01, size = self.size)
-            return np.random.normal(loc = np.random.choice(self.values), scale = 1, size = 1)
-        #return np.random.normal(loc = np.random.choice(self.values), scale = 1, size = 1)
-        return np.random.choice(self.rewards)
+        return np.random.normal(loc = self.true_value, scale = 1, size = 1)
 
-    def update_count(self):
-        self.count += 1
-        return self.count
-
-    def update_value(self, value):
-        self.value += value
-
-
-class KBanditSolution:
+class KBanditProblem:
 
     def __init__(self, k_arms, variance, size, stationary, steps):
         self.k_arms = k_arms
@@ -35,35 +23,48 @@ class KBanditSolution:
         self.variance = variance
         self.size = size
         self.stationary = stationary
+        self.bandits = [Bandit(self.variance, self.size, self.stationary) for _ in range(self.k_arms)]
         self.reset()
 
     def reset(self):
-        self.bandits = [Bandit(self.variance, self.size, self.stationary) for _ in range(self.k_arms)]
+        self.Q = np.zeros(self.k_arms)
+        self.N = np.zeros(self.k_arms)
         self.average_reward = 0
         self.average_rewards = np.array([])
         self.optimal_percentage = 0
         self.optimal_percentages = np.array([])
 
+    def get_optimal_action(self):
+        return self.Q.argmax()
+
+    def update_values_stationary(self, stationary):
+        if not stationary:
+            values = np.random.normal(loc = 0, scale = 0.01, size = self.k_arms)
+            for i, bandit in zip(list(range(10)), self.bandits):
+                bandit.true_value += values[i]
+
+
+class KBanditSolution(KBanditProblem):
 
     def calculate_statistics(self, step, reward, action):
         self.average_reward += (1/step) * (reward - self.average_reward)
         self.average_rewards = np.append(self.average_rewards, self.average_reward)
-        self.optimal_percentage += (1/step) * ((1 if action == max(self.bandits, key = attrgetter('value')) else 0) - self.optimal_percentage)
+        self.optimal_percentage += (1/step) * ((1 if action == self.bandits.index(max(self.bandits, key=attrgetter('true_value'))) else 0) - self.optimal_percentage)
         self.optimal_percentages = np.append(self.optimal_percentages, self.optimal_percentage)
-
 
     def solve_Egreedy(self, e):
         for k in range(self.steps):
             explore = random.uniform(0,1) < e
             if explore: # go to explore, else exploite
-                action = np.random.choice(self.bandits)
+                action = np.random.choice(self.k_arms) # the number of the action
             else: # exploit
-                action = max(self.bandits, key=attrgetter('value'))
-
-            reward = action.get_reward()
-            count = action.update_count()
-            value = (1/count) * (reward - action.value)
-            action.update_value(value)
+                action = self.get_optimal_action() # the number of the action
+            
+            self.update_values_stationary(stationary)
+            reward = self.bandits[action].get_reward()
+            self.N[action] += 1
+            value = (1/self.N[action]) * (reward -self.Q[action])
+            self.Q[action] += value
             self.calculate_statistics(k+1, reward, action)
 
 if __name__ == "__main__":
@@ -71,23 +72,27 @@ if __name__ == "__main__":
     variance = 1
     k= 10 # 10-armed bandits
     size = 1000 # number of gaussian distribution
-    stationary = 1
+    stationary = 0
 
     # stationary
     bandit = KBanditSolution(k, variance, size, stationary, steps)
     bandit.solve_Egreedy(0)
     avgR1 = bandit.average_rewards
     optV1 = bandit.optimal_percentages
+    print([bandit.true_value for bandit in bandit.bandits])
+    print(bandit.Q)
 
     bandit.reset()
     bandit.solve_Egreedy(0.01)
     avgR2 = bandit.average_rewards
     optV2 = bandit.optimal_percentages
+    print(bandit.Q)
 
     bandit.reset()
     bandit.solve_Egreedy(0.1)
     avgR3 = bandit.average_rewards
     optV3 = bandit.optimal_percentages
+    print(bandit.Q)
     
     plt.figure(1)
     plt.plot(list(range(steps)), avgR1, 'r', list(range(steps)), avgR2, 'b', list(range(steps)), avgR3, 'g')
