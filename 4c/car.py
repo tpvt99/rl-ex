79f2396e3bbe5d8768d8f7bb836b48fa22cc6faf
Tran Phong <tranphong96.hbk@gmail.com>
2018-08-14 05:29:07 -0700

    update

--- 4c/car.py
+++ 4c/car.py
@@ -43,11 +43,19 @@ def generate_poission_prob():
 @ action positive for moving cars from pos 1 to pos 2
          negative for moving cars from pos 2 to pos 1
 
-@ current_state the current states
-@ state_values the matrix containings all values of state
+@ current_state the current states, list of 2 numbers [a,b] a is number of cars in pos1, b is number of cars in pos2
+@ state_values the matrix containings all values of state. shape[0] is height, corresponding to number of cars in position 1
 """
 def expected_value(current_state, state_values, action):
-    
+    out = 0
+
+    next_temp_state = [max(0, min(MAX_CARS, current_state[0] - action)), max(0, min(MAX_CARS, current_state[1] + action))]
+    outs += (-2 * math.abs(action) + DISCOUNT * state_values[next_temp_state[0], next_temp_state[1]]
+
+    for pos1 in state_values.shape[0]:
+        for pos2 in state_values.shape[1]:
+            
+        
 
 
 
